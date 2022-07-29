51ab2716d1b6d642c9512a02317acbd8ad11eed4
MaryChiodelli <marychiodelli97@gmail.com>
2022-07-29 18:40:00 +0200

    fix number of cells

--- bonus/js/app.js
+++ bonus/js/app.js
@@ -3,26 +3,41 @@ const buttonEl = document.querySelector('.main-header button');
 const gridEl = document.querySelector('main .grid');
 gridEl.innerHTML = ''
 
-const levelMode = selectEl.value;
-let gridSize = 9;
-
-if (levelMode === 0) {
-    gridSize = 10;
-} else if (levelMode === 2) {
-    gridSize = 7;
-}
-
-const numOfCells = gridSize ** 2;
-
 buttonEl.addEventListener('click', function () {
-    console.log(selectEl.value, gridSize);
+    const levelMode = getLevelMode(selectEl);
+    let gridSize = 9;
+
+    if (levelMode === 'easy') {
+        gridSize = 10;
+    } else if (levelMode === 'hard') {
+        gridSize = 7;
+    }
+    
+    const numOfCells = gridSize ** 2;
     // generare griglia 10 x 10
     for (let i = 0; i < numOfCells; i++) {
         const square = createSquareEl();
         square.innerHTML = i + 1;
+        if (levelMode === 'easy') {
+            square.classList.add('cell-10');
+        } else if (levelMode === 'hard') {
+            square.classList.add('cell-7');
+        } else {
+            square.classList.add('cell-9');
+        }
         gridEl.append(square);
     }
 });
+
+function getLevelMode(input) {
+    const level = parseInt(input.value);
+    if (level === 0) {
+        return 'easy';
+    } else if (level === 2) {
+        return 'hard';
+    }
+    return 'medium';
+}
 
 function createSquareEl() {
     const square = document.createElement('div');
