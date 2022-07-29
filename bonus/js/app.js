5c500de7d6178fc72acbece0393a8980d814de76
MaryChiodelli <marychiodelli97@gmail.com>
2022-07-29 17:24:24 +0200

    add select

--- bonus/js/app.js
+++ bonus/js/app.js
@@ -1,11 +1,21 @@
+const selectEl = document.querySelector('.main-header select');
 const buttonEl = document.querySelector('.main-header button');
 const gridEl = document.querySelector('main .grid');
 gridEl.innerHTML = ''
 
-const gridSize = 10;
+const levelMode = selectEl.value;
+let gridSize = 9;
+
+if (levelMode === 0) {
+    gridSize = 10;
+} else if (levelMode === 2) {
+    gridSize = 7;
+}
+
 const numOfCells = gridSize ** 2;
 
 buttonEl.addEventListener('click', function () {
+    console.log(selectEl.value, gridSize);
     // generare griglia 10 x 10
     for (let i = 0; i < numOfCells; i++) {
         const square = createSquareEl();
