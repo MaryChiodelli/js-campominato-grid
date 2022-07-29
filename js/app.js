79b4d00b17e04290b3830936cc1dc647e289095d
MaryChiodelli <marychiodelli97@gmail.com>
2022-07-29 16:29:46 +0200

    change bg-color on click

--- js/app.js
+++ js/app.js
@@ -8,12 +8,21 @@ const numOfCells = gridSize ** 2;
 buttonEl.addEventListener('click', function () {
     // generare griglia 10 x 10
     for (let i = 0; i < numOfCells; i++) {
-        const cell = document.createElement('div');
-        cell.classList.add('square');
-        gridEl.append(cell);
-        // console.log(i);
+        const square = createSquareEl();
+        square.innerHTML = i + 1;
+        gridEl.append(square);
     }
 });
 
-console.log(gridEl);
+function createSquareEl() {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.addEventListener('click', clickHandler);
+    return square;
+}
 
+function clickHandler() {
+    const square = this;
+    square.classList.add('active');
+    console.log(square.innerHTML);
+}
